72849e1ff0d902a02b872cf46eebd46806cc21cb
adrianrevilla009 <adrianrg1996@gmail.com>
2023-03-10 13:18:03 +0100

    sam init

---
+++ bookManagementApiAWSLambda/bookManagementApiFunction/src/main/java/masterCloudApps/api/bookManagementApiFunction/model/Book.java
@@ -0,0 +1,80 @@
+package masterCloudApps.api.bookManagementApiFunction.model;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public class Book {
+    private String id;
+    private String title;
+    private String resume;
+    private Userr author;
+    private String editorial;
+    private LocalDate publicationDate;
+    private List<Comment> commentList;
+
+    public Book() {}
+
+    public Book(String title, String resume, Userr author, String editorial, LocalDate publicationDate) {
+        this.title = title;
+        this.resume = resume;
+        this.author = author;
+        this.editorial = editorial;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getResume() {
+        return resume;
+    }
+
+    public void setResume(String resume) {
+        this.resume = resume;
+    }
+
+    public Userr getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(Userr author) {
+        this.author = author;
+    }
+
+    public String getEditorial() {
+        return editorial;
+    }
+
+    public void setEditorial(String editorial) {
+        this.editorial = editorial;
+    }
+
+    public LocalDate getPublicationDate() {
+        return publicationDate;
+    }
+
+    public void setPublicationDate(LocalDate publicationDate) {
+        this.publicationDate = publicationDate;
+    }
+
+    public List<Comment> getCommentList() {
+        return commentList;
+    }
+
+    public void setCommentList(List<Comment> commentList) {
+        this.commentList = commentList;
+    }
+}
